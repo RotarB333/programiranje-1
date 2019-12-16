d280fff1c722f6c2412cf8778409fcb00b014d62
RotarB333 <bor.rotar1@gmail.com>
2019-12-16 12:00:40 +0100

    Add files via upload

--- 11-deli-in-vladaj/vaje/deli_in_vladaj.py
+++ 11-deli-in-vladaj/vaje/deli_in_vladaj.py
@@ -1,4 +1,4 @@
-##############################################################################
+###############################################################################
 # Želimo definirati pivotiranje na mestu za tabelo [a]. Ker bi želeli
 # pivotirati zgolj dele tabele, se omejimo na del tabele, ki se nahaja med
 # indeksoma [start] in [end].
@@ -25,16 +25,28 @@
 #     3
 #     >>> a
 #     [10, 2, 0, 4, 11, 15, 17, 5, 18]
-##############################################################################
+###############################################################################
 
+import random
 
+def pivot(a, start,end) :
+    if end <= start :
+        return start
+    first_larger = start+1
+    for i in range(start, end+1) :
+        if a[i] < a[start]:
+            #swap i j : a[i],a[j] = a[j],a[i]
+            a[first_larger],a[i] = a[i],a[first_larger]
+            first_larger += 1
+    a[start], a[first_larger-1] = a[first_larger-1], a[start]
+    return first_larger-1
 
-##############################################################################
+###############################################################################
 # V tabeli želimo poiskati vrednost k-tega elementa po velikosti.
 #
 # Primer: Če je
 #
-# >>> a = [10, 4, 5, 15, 11, 3, 17, 2, 18]
+#     >>> a = [10, 4, 5, 15, 11, 3, 17, 2, 18]
 #
 # potem je tretji element po velikosti enak 5, ker so od njega manši elementi
 #  2, 3 in 4. Pri tem štejemo indekse od 0 naprej, torej je "ničti" element 2.
@@ -42,11 +54,30 @@
 # Sestavite funkcijo [kth_element(a, k)], ki v tabeli [a] poišče [k]-ti
 # element po velikosti. Funkcija sme spremeniti tabelo [a]. Cilj naloge je, da
 # jo rešite brez da v celoti uredite tabelo [a].
-##############################################################################
+###############################################################################
+
+def kth_element(a,k):
+    def kth(lower, upper):
+        candidate_i = pivot(a, lower, upper)
+        if candidate_i == k:
+            return a [candidate_i]
+        elif candidate_i < k:
+            return kth(candidate_i+1, upper)
+        else:
+            return kth(lower, candidate_i-1)
+    return kth(0, len(a)-1)
 
 
+def kth_element1(a,k) :
+    m = 0
+    for i in range(0, len(a)-1):
+        for j in range(0, len(a)-1):
+            if a[j] < a[i]:
+                m+=1
+        if k == m+1:
+            return a[i]    
 
-##############################################################################
+###############################################################################
 # Tabelo a želimo urediti z algoritmom hitrega urejanja (quicksort).
 #
 # Napišite funkcijo [quicksort(a)], ki uredi tabelo [a] s pomočjo pivotiranja.
@@ -55,7 +86,72 @@
 # Namig: Definirajte pomožno funkcijo [quicksort_part(a, start, end)], ki
 #        uredi zgolj del tabele [a].
 #
-#   >>> a = [10, 4, 5, 15, 11, 3, 17, 2, 18]
-#   >>> quicksort(a)
-#   [2, 3, 4, 5, 10, 11, 15, 17, 18]
-##############################################################################
+#     >>> a = [10, 4, 5, 15, 11, 3, 17, 2, 18]
+#     >>> quicksort(a)
+#     [2, 3, 4, 5, 10, 11, 15, 17, 18]
+###############################################################################
+
+def quicksort(a):
+    def qsort(a, s, e):
+        if e <= s:
+            return
+        p_i = pivot(a, s, e)
+        qsort(a, s, p_i-1)
+        qsort(a, p_i+1, e)
+    qsort(a, 0, len(a)-1)
+    return a
+
+def test_quicksort():
+    for _ in range(1000):
+        a = [random.randint(-100000,100000) for _ in range(1000)]
+        b1 = a[:]
+        b2 = a[:]
+        quicksort(b1)
+        b2.sort()
+        if b1 != b2:
+            return 'Not working, try ()'.format(a)
+
+
+###############################################################################
+# Če imamo dve urejeni tabeli, potem urejeno združeno tabelo dobimo tako, da
+# urejeni tabeli zlijemo. Pri zlivanju vsakič vzamemo manjšega od začetnih
+# elementov obeh tabel. Zaradi učinkovitosti ne ustvarjamo nove tabele, ampak
+# rezultat zapisujemo v že pripravljeno tabelo (ustrezne dolžine).
+# 
+# Funkcija naj deluje v času O(n), kjer je n dolžina tarčne tabele.
+# 
+# Sestavite funkcijo [zlij(target, begin, end, list_1, list_2)], ki v del 
+# tabele [target] med start in end zlije tabeli [list_1] in [list_2]. V primeru, 
+# da sta elementa v obeh tabelah enaka, naj bo prvi element iz prve tabele.
+# 
+# Primer:
+#  
+#     >>> list_1 = [1,3,5,7,10]
+#     >>> list_2 = [1,2,3,4,5,6,7]
+#     >>> target = [-1 for _ in range(len(list_1) + len(list_2))]
+#     >>> zlij(target, 0, len(target), list_1, list_2)
+#     >>> target
+#     [1,1,2,3,3,4,5,5,6,7,7,10]
+#
+###############################################################################
+
+#def zlij(target, begin, end, list_1, list_2):
+#    for i in range(0, len(target-1)):
+
+
+
+###############################################################################
+# Tabelo želimo urediti z zlivanjem (merge sort). 
+# Tabelo razdelimo na polovici, ju rekurzivno uredimo in nato zlijemo z uporabo
+# funkcije [zlij].
+#
+# Namig: prazna tabela in tabela z enim samim elementom sta vedno urejeni.
+#
+# Napišite funkcijo [mergesort(a)], ki uredi tabelo [a] s pomočjo zlivanja.
+# Za razliko od hitrega urejanja tu tabele lahko kopirate, zlivanje pa je 
+# potrebno narediti na mestu.
+#
+# >>> a = [10, 4, 5, 15, 11, 3, 17, 2, 18]
+# >>> mergesort(a)
+# [2, 3, 4, 5, 10, 11, 15, 17, 18]
+###############################################################################
